87ada69a4f11c7ab268497a43914cc57f2f0e5dd
GET Academy <terje@kolderup.net>
2021-01-16 21:23:27 +0100

    Update labyrinth.js

--- labyrinth.js
+++ labyrinth.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const labyrinthSize = 5;
 const cornerSize = 10;
 const wallToCornerRatio = 4;
+const directions = ['north', 'south', 'east', 'west'];
 const roomCount = labyrinthSize * labyrinthSize;
 const openDoors = {};
 openDoors['north0'] = true;
@@ -12,7 +13,35 @@ const pixels = calcWallSize(labyrinthSize + 1);
 canvas.setAttribute('width', pixels);
 canvas.setAttribute('height', pixels);
 
+generateLabyrinth();
 drawLabyrinth();
+
+function generateLabyrinth() {
+    const rooms = [];
+    rooms[0] = true;
+    const stack = [];
+    stack.push(0);
+
+    while (stack.length > 0) {
+        let currentRoom = stack.pop();
+        let neighbourNotVisited = getNeighbourNotVisited(currentRoom);
+    }
+}
+
+function getNeighbourNotVisited(roomIndex) {
+    shuffle(directions);
+    const rowIndex = Math.floor(roomIndex / labyrinthSize);
+    const colIndex = roomIndex % labyrinthSize;
+    for (let direction of directions) {
+        if (direction == 'north' && rowIndex > 0 && !rooms[roomIndex - labyrinthSize]) return roomIndex - labyrinthSize
+        if (direction == 'west' && rowIndex > 0 && !rooms[roomIndex - labyrinthSize]) return roomIndex - labyrinthSize
+        if (direction == 'east' && rowIndex > 0 && !rooms[roomIndex - labyrinthSize]) return roomIndex - labyrinthSize
+        if (direction == 'south' && rowIndex > 0 && !rooms[roomIndex - labyrinthSize]) return roomIndex - labyrinthSize
+    }
+}
+
+function 
+
 function drawLabyrinth() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let roomIndex = 0; roomIndex < roomCount; roomIndex++) {
@@ -58,4 +87,23 @@ function drawCorner(rowIndex, colIndex, addX, addY) {
 
 function calcWallSize(index) {
     return index * wallSize + (index + 1) * cornerSize;
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 }
