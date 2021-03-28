4fe12b4ad50d5df849eb4230cc4052fefa619839
GET Academy <terje@kolderup.net>
2021-03-28 19:01:34 +0200

    Update labyrinth.js

--- labyrinth.js
+++ labyrinth.js
@@ -1,39 +1,40 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-let labyrinthSize;
-let cornerSize = 20;
-const wallToCornerRatio = 4;
-const directions = ['opp', 'ned', 'høyre', 'venstre'];
-const oppositeDirection = {
-    'opp': 'ned',
-    'ned': 'opp',
-    'venstre': 'høyre',
-    'høyre': 'venstre',
-};
-let roomCount;
-const openWalls = {};
-const wallSize = cornerSize * wallToCornerRatio;
-let pixels;
-let character = {
-    roomIndex: 0,
-    direction: 'ned',
-};
-const commandQueue = [];
-const timer = null;
+const globals = {};
 
 function init(size) {
-    labyrinthSize = size;
-    roomCount = labyrinthSize * labyrinthSize;
-    openWalls['opp0'] = true;
-    openWalls['ned' + (roomCount - 1)] = true;
-    pixels = calcWallSize(labyrinthSize + 1);
-    canvas.setAttribute('width', pixels);
-    canvas.setAttribute('height', pixels);
+    globals.canvas = document.getElementById("myCanvas");
+    globals.ctx = globals.canvas.getContext("2d");
+    const model = globals.model = {};
+    model.labyrinthSize = size;
+    model.cornerSize = 20;
+    model.wallToCornerRatio = 4;
+    model.directions = ['opp', 'ned', 'høyre', 'venstre'];
+    model.oppositeDirection = {
+        'opp': 'ned',
+        'ned': 'opp',
+        'venstre': 'høyre',
+        'høyre': 'venstre',
+    };
+    model.openWalls = {};
+    model.wallSize = model.cornerSize * model.wallToCornerRatio;
+    model.character = {
+        roomIndex: 0,
+        direction: 'ned',
+    };
+    model.commandQueue = [];
+    globals.timer = null;
+
+    model.roomCount = model.labyrinthSize * model.labyrinthSize;
+    model.openWalls['opp0'] = true;
+    model.openWalls['ned' + (model.roomCount - 1)] = true;
+    model.pixels = calcWallSize(model.labyrinthSize + 1);
+    globals.canvas.setAttribute('width', model.pixels);
+    globals.canvas.setAttribute('height', model.pixels);
     generateLabyrinth();
     drawLabyrinth();
 }
 
 function generateLabyrinth() {
+    const model = globals.model;
     const rooms = [];
     rooms[0] = true;
     const stack = [];
@@ -44,70 +45,74 @@ function generateLabyrinth() {
         let neighbourNotVisited = getNeighbourNotVisited(currentRoom, rooms);
         if (neighbourNotVisited === null) continue;
         stack.push(currentRoom);
-        openWalls[neighbourNotVisited.direction + currentRoom] = true;
-        const directionFromNeighbour = oppositeDirection[neighbourNotVisited.direction];
-        openWalls[directionFromNeighbour + neighbourNotVisited.roomIndex] = true;
+        model.openWalls[neighbourNotVisited.direction + currentRoom] = true;
+        const directionFromNeighbour = model.oppositeDirection[neighbourNotVisited.direction];
+        model.openWalls[directionFromNeighbour + neighbourNotVisited.roomIndex] = true;
         rooms[neighbourNotVisited.roomIndex] = true;
         stack.push(neighbourNotVisited.roomIndex);
     }
 }
 
 function getNeighbourNotVisited(roomIndex, rooms) {
-    shuffle(directions);
-    const rowIndex = Math.floor(roomIndex / labyrinthSize);
-    const colIndex = roomIndex % labyrinthSize;
-    for (let direction of directions) {
-        if (direction == 'opp' && rowIndex > 0 && !rooms[roomIndex - labyrinthSize])
-            return { roomIndex: roomIndex - labyrinthSize, direction };
+    const model = globals.model;
+    shuffle(model.directions);
+    const rowIndex = Math.floor(roomIndex / model.labyrinthSize);
+    const colIndex = roomIndex % model.labyrinthSize;
+    for (let direction of model.directions) {
+        if (direction == 'opp' && rowIndex > 0 && !rooms[roomIndex - model.labyrinthSize])
+            return { roomIndex: roomIndex - model.labyrinthSize, direction };
         if (direction == 'venstre' && colIndex > 0 && !rooms[roomIndex - 1])
             return { roomIndex: roomIndex - 1, direction };
-        if (direction == 'høyre' && colIndex < labyrinthSize - 1 && !rooms[roomIndex + 1])
+        if (direction == 'høyre' && colIndex < model.labyrinthSize - 1 && !rooms[roomIndex + 1])
             return { roomIndex: roomIndex + 1, direction };
-        if (direction == 'ned' && rowIndex < labyrinthSize - 1 && !rooms[roomIndex + labyrinthSize])
-            return { roomIndex: roomIndex + labyrinthSize, direction };
+        if (direction == 'ned' && rowIndex < model.labyrinthSize - 1 && !rooms[roomIndex + model.labyrinthSize])
+            return { roomIndex: roomIndex + model.labyrinthSize, direction };
     }
     return null;
 }
 
 function drawLabyrinth() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let roomIndex = 0; roomIndex < roomCount; roomIndex++) {
+    const model = globals.model;
+    globals.ctx.clearRect(0, 0, globals.canvas.width, globals.canvas.height);
+    for (let roomIndex = 0; roomIndex < model.roomCount; roomIndex++) {
         drawRoom(roomIndex);
     }
     drawCharacter();
 }
 
 function drawCharacter() {
-    const rowIndex = Math.floor(character.roomIndex / labyrinthSize);
-    const colIndex = character.roomIndex % labyrinthSize;
-    let x = calcWallSize(colIndex) + 1.5 * cornerSize;
-    let y = calcWallSize(rowIndex) + 1.5 * cornerSize;
-    const size = wallSize - cornerSize;
+    const ctx = globals.ctx;
+    const model = globals.model;
+    const rowIndex = Math.floor(model.character.roomIndex / model.labyrinthSize);
+    const colIndex = model.character.roomIndex % model.labyrinthSize;
+    let x = calcWallSize(colIndex) + 1.5 * model.cornerSize;
+    let y = calcWallSize(rowIndex) + 1.5 * model.cornerSize;
+    const size = model.wallSize - model.cornerSize;
     ctx.beginPath();
-    if (character.direction == 'høyre') {
+    if (model.character.direction == 'høyre') {
         ctx.moveTo(x, y);
         ctx.lineTo(x + size, y + size / 2);
         ctx.lineTo(x, y + size);
-    } else if (character.direction == 'venstre') {
+    } else if (model.character.direction == 'venstre') {
         ctx.moveTo(x, y + size / 2);
         ctx.lineTo(x + size, y);
         ctx.lineTo(x + size, y + size);
-    } else if (character.direction == 'opp') {
+    } else if (model.character.direction == 'opp') {
         ctx.moveTo(x + size / 2, y);
         ctx.lineTo(x, y + size);
         ctx.lineTo(x + size, y + size);
-    } else if (character.direction == 'ned') {
+    } else if (model.character.direction == 'ned') {
         ctx.moveTo(x, y);
         ctx.lineTo(x + size, y);
         ctx.lineTo(x + size / 2, y + size);
     }
     ctx.fill();
-
 }
 
 function drawRoom(roomIndex) {
-    const rowIndex = Math.floor(roomIndex / labyrinthSize);
-    const colIndex = roomIndex % labyrinthSize;
+    const model = globals.model;
+    const rowIndex = Math.floor(roomIndex / model.labyrinthSize);
+    const colIndex = roomIndex % model.labyrinthSize;
     const isFirstCol = colIndex == 0;
     const isFirstRow = rowIndex == 0;
     if (isFirstRow) drawWall(rowIndex, colIndex, roomIndex, 'opp');
@@ -121,34 +126,37 @@ function drawRoom(roomIndex) {
 }
 
 function drawWall(rowIndex, colIndex, roomIndex, direction) {
+    const model = globals.model;
     const doorKey = direction + roomIndex;
-    if (openWalls[doorKey]) return;
+    if (model.openWalls[doorKey]) return;
     const isDoorHorizontal = direction == 'opp' || direction == 'ned';
     let x = calcWallSize(colIndex);
     let y = calcWallSize(rowIndex);
-    if (direction == 'opp') { x += cornerSize; }
-    if (direction == 'venstre') { y += cornerSize; }
+    if (direction == 'opp') { x += model.cornerSize; }
+    if (direction == 'venstre') { y += model.cornerSize; }
     if (direction == 'høyre') {
-        x += wallSize + cornerSize;
-        y += cornerSize;
+        x += model.wallSize + model.cornerSize;
+        y += model.cornerSize;
     }
     if (direction == 'ned') {
-        y += wallSize + cornerSize;
-        x += cornerSize;
+        y += model.wallSize + model.cornerSize;
+        x += model.cornerSize;
     }
-    const width = isDoorHorizontal ? wallSize : cornerSize;
-    const height = isDoorHorizontal ? cornerSize : wallSize;
-    ctx.fillRect(x, y, width, height);
+    const width = isDoorHorizontal ? model.wallSize : model.cornerSize;
+    const height = isDoorHorizontal ? model.cornerSize : model.wallSize;
+    globals.ctx.fillRect(x, y, width, height);
 }
 
 function drawCorner(rowIndex, colIndex, addX, addY) {
-    let x = calcWallSize(colIndex) + addX * (cornerSize + wallSize);
-    let y = calcWallSize(rowIndex) + addY * (cornerSize + wallSize);
-    ctx.fillRect(x, y, cornerSize, cornerSize);
+    const model = globals.model;
+    let x = calcWallSize(colIndex) + addX * (model.cornerSize + model.wallSize);
+    let y = calcWallSize(rowIndex) + addY * (model.cornerSize + model.wallSize);
+    globals.ctx.fillRect(x, y, model.cornerSize, model.cornerSize);
 }
 
 function calcWallSize(index) {
-    return index * wallSize + (index + 1) * cornerSize;
+    const model = globals.model;
+    return index * model.wallSize + (index + 1) * model.cornerSize;
 }
 
 function shuffle(array) {
@@ -177,33 +185,36 @@ function shuffle(array) {
 // }
 
 function snuHøyre() {
+    const model = global.model;
     //pause(400);
-    character.direction = {
+    model.character.direction = {
         'opp': 'høyre',
         'høyre': 'ned',
         'ned': 'venstre',
         'venstre': 'opp',
-    }[character.direction];
+    }[model.character.direction];
     drawLabyrinth();
-    return character.direction;
+    return model.character.direction;
 }
 
 function gå() {
+    const model = global.model;
     //pause(400);
-    if (!openWalls[character.direction + character.roomIndex]) return false;
-    const position = getRowAndCol(character.roomIndex);
+    if (!model.openWalls[model.character.direction + model.character.roomIndex]) return false;
+    const position = getRowAndCol(model.character.roomIndex);
 
-    if (character.direction == 'opp' && position.rowIndex > 0) character.roomIndex -= labyrinthSize;
-    else if (character.direction == 'venstre' && position.colIndex > 0) character.roomIndex -= 1;
-    else if (character.direction == 'høyre' && position.colIndex < labyrinthSize - 1) character.roomIndex += 1;
-    else if (character.direction == 'ned' && position.rowIndex < labyrinthSize - 1) character.roomIndex += labyrinthSize;
+    if (model.character.direction == 'opp' && position.rowIndex > 0) model.character.roomIndex -= model.labyrinthSize;
+    else if (model.character.direction == 'venstre' && position.colIndex > 0) model.character.roomIndex -= 1;
+    else if (model.character.direction == 'høyre' && position.colIndex < model.labyrinthSize - 1) model.character.roomIndex += 1;
+    else if (model.character.direction == 'ned' && position.rowIndex < model.labyrinthSize - 1) model.character.roomIndex += model.labyrinthSize;
     drawLabyrinth();
     return true;
 }
 
 function getRowAndCol(roomIndex) {
-    const rowIndex = Math.floor(roomIndex / labyrinthSize);
-    const colIndex = roomIndex % labyrinthSize;
+    const model = global.model;
+    const rowIndex = Math.floor(roomIndex / model.labyrinthSize);
+    const colIndex = roomIndex % model.labyrinthSize;
     return { rowIndex, colIndex };
 }
 
@@ -226,5 +237,6 @@ function følgVeggx() {
 }
 
 function erVedUtgang() {
-    return character.roomIndex == labyrinthSize * labyrinthSize - 1;
+    const model = global.model;
+    return model.character.roomIndex == model.labyrinthSize * model.labyrinthSize - 1;
 }
