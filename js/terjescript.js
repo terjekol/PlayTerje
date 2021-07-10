5093bfbbbf17dfa6df3c6f11b7792e8170c77ebe
GET Academy <terje@kolderup.net>
2021-07-10 14:29:02 +0200

    Update terjescript.js

--- js/terjescript.js
+++ js/terjescript.js
@@ -1,15 +1,7 @@
 class TerjeScript {
     constructor(svgId) {
         this.svgId = svgId;
-        this.appContext = {
-            path: '',
-            programCounterIndex: 0,
-            state: {
-                x: 0,
-                y: 0,
-            },
-        };
-        const pathCommand = letter => function(args)  {
+        const pathCommand = letter => function (args) {
             this.appContext.path += `${letter} ${args.x} ${args.y} `;
             this.appContext.state.x = args.x;
             this.appContext.state.y = args.y;
@@ -18,48 +10,72 @@ class TerjeScript {
             hoppTil: {
                 name: 'Hopp til punktet',
                 params: ['x', 'y'],
-                impl: pathCommand('M')
+                impl: pathCommand('M').bind(this),
             },
             hopp: {
                 name: 'Hopp',
                 params: ['x', 'y'],
-                impl: pathCommand('m')
+                impl: pathCommand('m').bind(this),
             },
             tegnLinjeTil: {
                 name: 'Tegn linje til punktet',
                 params: ['x', 'y'],
-                impl: pathCommand('L')
+                impl: pathCommand('L').bind(this),
             },
             tegnLinje: {
                 name: 'Tegn linje',
                 params: ['x', 'y'],
-                impl: pathCommand('l')
+                impl: pathCommand('l').bind(this),
             },
             goto: {
                 nameTxt: 'Gå til ',
                 nameJs: 'tegnLinjeTilPunkt',
                 params: ['instructionId'],
-                programCounter(args){
+                programCounter(args) {
                     const id = args.instructionId;
-                    this.appContext.programCounterIndex = this.program.main.findIndex(instruction=>instruction.id===id);
+                    this.appContext.programCounterIndex = this.program.main.findIndex(instruction => instruction.id === id);
                 }
             }
         };
         this.program = {
             main: [
                 { id: 'bbb', command: this.commands.tegnLinje, args: { x: 10, y: 0 } },
-                { id: 'ccc', command: this.commands.lineTo, args: { x: 0, y: 10 } },
-                { id: 'ddd', command: this.commands.goto, args: { instructionId: 'bbb' } },
+                { id: 'ccc', command: this.commands.tegnLinje, args: { x: 0, y: 10 } },
+                //{ id: 'ddd', command: this.commands.goto, args: { instructionId: 'bbb' } },
             ],
             custom: {
 
             }
         }
     }
+    initAppContext() {
+        this.appContext = {
+            path: 'M 0 0 ',
+            programCounterIndex: 0,
+            state: {
+                x: 0,
+                y: 0,
+            },
+        };
+    }
+
     uniqueIdentifierPart() {
         return (0x10000 | Math.random() * 0x10000).toString(16).substr(1);
     }
     uniqueIdentifier(tmp) {
         return tmp && tmp.length === 32 ? tmp : this.random128hex((tmp || '') + this.uniqueIdentifierPart());
+    }
+    run() {
+        this.initAppContext();
+        for (let step of this.program.main) {
+            step.command.impl(step.args);
+        }
+        document.getElementById(this.svgId).innerHTML = `
+            <path 
+                d="${this.appContext.path}" 
+                stroke="green"
+                fill="none"
+                />
+            `;
     }
 }
